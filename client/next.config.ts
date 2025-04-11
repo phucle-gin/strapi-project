da53a195d4edd863d0823b92bc4daf28ec7c2940
Henry Le <henryle-dev>
2025-04-12 02:43:04 +1000

    Fixed next conf

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,14 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**/*",
-      },
-    ],
+   remotePatterns: [
+    {
+      protocol: "http",
+      hostname: "localhost",
+      port: "1337",
+      pathname: "/uploads/**",
+    },
+    {
+      protocol: "https",
+      hostname: "strapi-project-production-ff3b.up.railway.app",
+      pathname: "/uploads/**",
+    },
+  ],
   },
 };
 
