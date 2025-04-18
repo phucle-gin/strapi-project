57251785513724e2bc64961574a0662532d2d00a
Henry Le <henryle-dev>
2025-04-18 21:14:11 +1000

    Fixed favicon and domain

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,21 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-   remotePatterns: [
-    {
-      protocol: "http",
-      hostname: "localhost",
-      port: "1337",
-      pathname: "/uploads/**",
-    },
-    {
-      protocol: "https",
-      hostname: "strapi-project-production-ff3b.up.railway.app",
-      pathname: "/uploads/**",
-    },
-  ],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "strapi-project-production-ff3b.up.railway.app",
+        pathname: "/uploads/**",
+      },
+    ],
+    domains: ['res.cloudinary.com'], // ← move this outside remotePatterns
   },
 };
 
